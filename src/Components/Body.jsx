069b97e3ac9c39fe1aa40a3b8fb22c58105acddc
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-04-14 00:25:18 -0800

    completed with authentication

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,18 +1,20 @@
 import { useEffect } from "react"
 import Browse from "./Browse"
 import Login from "./Login"
-import { BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter,Routes,Route,} from "react-router-dom"
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../utils/firebase"
 import { useDispatch } from 'react-redux';
 import {addUser, removeUser} from '../utils/userSlice';
 const Body = () => {
   const dispatch=useDispatch();
+
   useEffect(()=>{
     onAuthStateChanged(auth, (user) => {
       if (user) {
         const {uid,email,displayName,photoURL} = user;
         dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}));
+        
       } else {
       dispatch(removeUser());
       }
